6279c1a86240d7c779fb6c8a42b633096559e8d7
Nikita Malinkin <nick.work.cs@gmail.com>
2026-08-12 21:05:15 +0300

    Initial commit

---
+++ src/main/java/ru/project/orderprocessingsystem/OrderProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.project.orderprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProcessingSystemApplication.class, args);
+    }
+
+}
